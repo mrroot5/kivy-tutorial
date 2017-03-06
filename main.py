ad9558e3a452dbf9504e31c4b8fa03aaa51ed34e
erknrio <erknrio@gmail.com>
2017-03-06 13:40:42 +0000

    Added nested grid layouts

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# mygrid.py
 # Py modules
 import requests
 # Kivy imports
@@ -7,11 +6,11 @@ import kivy
 kivy.require('1.9.1') # replace with your current kivy version !
 # Kivy modules
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 import kivy.uix.gridlayout as gridl
-# from kivy.properties import ObjectProperty
-# from kivy.uix.label import Label
-# from kivy.uix.button import Button
 
+class VideoGrid(gridl.GridLayout):
+    pass
 class ButtonsGrid(gridl.GridLayout):
     apiURL = 'https://api.github.com/'
     def forward(self, btn):
@@ -35,9 +34,12 @@ class ButtonsGrid(gridl.GridLayout):
         # response = r.json()
         # print(response)
 
+class ContainerBox(BoxLayout):
+    pass
+
 class ButtonsGridApp(App):
     def build(self):
-        return ButtonsGrid()
+        return ContainerBox()
 
 if __name__ == '__main__':
     ButtonsGridApp().run()
