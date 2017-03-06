3f407d5798c515616ba915aef2ea8c9e0d0d9729
erknrio <erknrio@gmail.com>
2017-03-06 12:29:06 +0000

    First click event tests

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
+#!/usr/bin/env python3
+# mygrid.py
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.9.1') # replace with your current kivy version !
 
 from kivy.app import App
 import kivy.uix.gridlayout as gridl
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+# from kivy.uix.label import Label
+# from kivy.uix.button import Button
 
+class ButtonsGrid(gridl.GridLayout):
+    pass
 
-class ButtonsGrid(App):
+class ButtonsGridApp(App):
+    def clicked(instance):
+        print('The button <%s> is being pressed' % instance.text)
+
     def build(self):
-        layout = gridl.GridLayout(cols=3, rows=3)
-        layout.add_widget(Button(text='0,0'))
-        layout.add_widget(Button(text='0,1'))
-        layout.add_widget(Button(text='0,2'))
-        layout.add_widget(Button(text='1,0'))
-        layout.add_widget(Button(text='1,1'))
-        layout.add_widget(Button(text='1,2'))
-        layout.add_widget(Button(text='2,0'))
-        layout.add_widget(Button(text='2,1'))
-        layout.add_widget(Button(text='2,2'))
-        return layout
-
+        return ButtonsGrid()
 
 if __name__ == '__main__':
-    ButtonsGrid().run()
+    ButtonsGridApp().run()
