a3b59704762f45c75d3cbe2cf97cb35c9d90be5b
erknrio <erknrio@gmail.com>
2017-03-06 13:13:54 +0000

    Added prints to click events

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 # mygrid.py
+# Py modules
+import requests
+# Kivy imports
 import kivy
 kivy.require('1.9.1') # replace with your current kivy version !
-
+# Kivy modules
 from kivy.app import App
 import kivy.uix.gridlayout as gridl
+# from kivy.properties import ObjectProperty
 # from kivy.uix.label import Label
 # from kivy.uix.button import Button
 
 class ButtonsGrid(gridl.GridLayout):
-    pass
+    apiURL = 'https://api.github.com/'
+    def forward(self, btn):
+        print(btn.text)
+        # r = requests.get(self.apiURL + "events")
+        # response = r.json()
+        # print(response)
+    def right(self, btn):
+        print(btn.text)
+        # r = requests.get(self.apiURL + "events")
+        # response = r.json()
+        # print(response)
+    def back(self, btn):
+        print(btn.text)
+        # r = requests.get(self.apiURL + "events")
+        # response = r.json()
+        # print(response)
+    def left(self, btn):
+        print(btn.text)
+        # r = requests.get(self.apiURL + "events")
+        # response = r.json()
+        # print(response)
 
 class ButtonsGridApp(App):
-    def clicked(instance):
-        print('The button <%s> is being pressed' % instance.text)
-
     def build(self):
         return ButtonsGrid()
 
