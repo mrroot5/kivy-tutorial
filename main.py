96d27d6cd4e859699404dc315f99e9876dc45493
erknrio <erknrio@gmail.com>
2017-03-07 13:10:12 +0000

    Added functionallity

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.9.1')
 
 from kivy.app import App
 import kivy.uix.gridlayout as gridl
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-
 
 class ButtonsGrid(App):
     def build(self):
         layout = gridl.GridLayout(cols=3, rows=3)
+        # Boton por defecto
         layout.add_widget(Button(text='0,0'))
-        layout.add_widget(Button(text='0,1'))
+        # Boton personalizado:
+        # Podemos agregar todas las propiedades
+        # directamente al constructor
+        customBtn = Button(id="btnUp", text='Click Me!!')
+        # O podemos agregarlas posteriormente
+        customBtn.bold=True
+        customBtn.background_color=[255, 0, 0, 1]
+        customBtn.bind(on_press=self.clicked)
+        layout.add_widget(customBtn)
         layout.add_widget(Button(text='0,2'))
         layout.add_widget(Button(text='1,0'))
         layout.add_widget(Button(text='1,1'))
@@ -20,7 +27,9 @@ class ButtonsGrid(App):
         layout.add_widget(Button(text='2,1'))
         layout.add_widget(Button(text='2,2'))
         return layout
-
+        
+    def clicked(self, btn):
+        print(btn.text)
 
 if __name__ == '__main__':
     ButtonsGrid().run()
