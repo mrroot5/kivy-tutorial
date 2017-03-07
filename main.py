2437da053945a7443e6ff00c4fc74a876bc4afae
erknrio <erknrio@gmail.com>
2017-03-07 12:35:05 +0000

    Added video loaded status info

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # Py modules
 from requests import get
-# import iconfonts
 # Kivy imports
 import kivy
 kivy.require('1.9.1') # replace with your current kivy version !
@@ -12,46 +11,45 @@ from kivy.uix.boxlayout import BoxLayout
 import kivy.uix.gridlayout as gridl
 
 class VideoGrid(gridl.GridLayout):
-    pass
+    def video_loaded(self):
+        status_bar = self.parent.children[0].ids.status_text
+        self.parent.change_status(status_bar, 'Video Loaded')
+
 class ButtonsGrid(gridl.GridLayout):
     apiURL = 'https://api.github.com/'
     def do_request(self):
         r = get(self.apiURL + "events")
-        response = r.json()
-        print(response)
-
-    def change_status(self, element, new_text = 'Status'):
-        element.text = new_text
+        return r.json()
 
     def forward(self, btn):
         status_bar = self.parent.children[0].ids.status_text
-        self.change_status(status_bar, btn.text)
-        # self.do_request()
+        self.parent.change_status(status_bar, btn.text)
+        self.parent.change_status(status_bar, self.do_request())
 
     def right(self, btn):
         status_bar = self.parent.children[0].ids.status_text
-        self.change_status(status_bar, btn.text)
-        # self.do_request()
+        self.parent.change_status(status_bar, btn.text)
+        # self.parent.change_status(status_bar, self.do_request())
 
     def back(self, btn):
         status_bar = self.parent.children[0].ids.status_text
-        self.change_status(status_bar, btn.text)
-        # self.do_request()
+        self.parent.change_status(status_bar, btn.text)
+        # self.parent.change_status(status_bar, self.do_request())
 
     def left(self, btn):
         status_bar = self.parent.children[0].ids.status_text
-        self.change_status(status_bar, btn.text)
-        # self.do_request()
+        self.parent.change_status(status_bar, btn.text)
+        # self.parent.change_status(status_bar, self.do_request())
 
 class StatusBar(ScrollView):
     pass
 
 class ContainerBox(BoxLayout):
-    pass
+    def change_status(self, element, new_text = 'Status'):
+        element.text = new_text
 
 class ButtonsGridApp(App):
     def build(self):
-        # iconfonts.register('default_font', 'iconfont_sample.ttf', 'iconfont_sample.fontd')
         return ContainerBox()
 
 if __name__ == '__main__':
